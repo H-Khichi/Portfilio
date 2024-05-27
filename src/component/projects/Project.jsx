45d6e164ea1abd0430e2810e86d72ca3b946b290
H-Khichi <145919359+H-Khichi@users.noreply.github.com>
2024-05-27 16:22:43 +0500

    updaterd

--- src/component/projects/Project.jsx
+++ src/component/projects/Project.jsx
@@ -6,6 +6,8 @@ import p3 from './../../images/3.png'
 import p4 from './../../images/4.png'
 import p5 from './../../images/5.png'
 import p6 from './../../images/6.png'
+import p7 from './../../images/7.png'
+import p8 from './../../images/8.png'
 
 const Project = () => {
     return (
@@ -16,6 +18,27 @@ const Project = () => {
                         <h1>My Projects</h1>
                     </div>
                     <div className="container">
+                    <div className="project-even">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <img src={p8} alt="" width={'90%'} />
+                                </div>
+                                <div className="col-md-6 mt-sm-3 ">
+                                    <h1>Core Solution And <br /> Networking Hub</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="project-odd">
+                            <div className="row">
+                                <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
+                                    <h1>Quiz App</h1>
+                                </div>
+                                <div className="col-md-6 order-1 order-md-2">
+                                    <img src={p7} alt="" width={'90%'} />
+                                </div>
+
+                            </div>
+                        </div>
                         <div className="project-even">
                             <div className="row">
                                 <div className="col-md-6 ">
@@ -28,13 +51,13 @@ const Project = () => {
                         </div>
                         <div className="project-odd">
                             <div className="row">
-                            <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
+                                <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
                                     <h1>Stics</h1>
                                 </div>
                                 <div className="col-md-6 order-1 order-md-2">
                                     <img src={p2} alt="" width={'90%'} />
                                 </div>
-                               
+
                             </div>
                         </div>
                         <div className="project-even">
@@ -49,13 +72,13 @@ const Project = () => {
                         </div>
                         <div className="project-odd">
                             <div className="row">
-                            <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
-                                    <h1>Netflix </h1>
+                                <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
+                                    <h1>Netflix (Clone)</h1>
                                 </div>
                                 <div className="col-md-6 order-1 order-md-2">
                                     <img src={p4} alt="" width={'90%'} />
                                 </div>
-                                
+
                             </div>
                         </div>
                         <div className="project-even">
@@ -64,22 +87,22 @@ const Project = () => {
                                     <img src={p5} alt="" width={'90%'} />
                                 </div>
                                 <div className="col-md-6 mt-sm-3 ">
-                                    <h1>Hotel</h1>
+                                    <h1>Hotel (Clone)</h1>
                                 </div>
                             </div>
                         </div>
                         <div className="project-odd">
                             <div className="row">
-                            <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
-                                    <h1>Architecture</h1>
+                                <div className="col-md-6 order-2 order-md-1 mt-sm-3 ">
+                                    <h1>Architecture (Clone)</h1>
                                 </div>
                                 <div className="col-md-6 order-1 order-md-2">
                                     <img src={p6} alt="" width={'90%'} />
                                 </div>
-                                
+
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </section>
